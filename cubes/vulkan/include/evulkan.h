0c508415dacb11143fcd11552f88afb72fd86781
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-04-09 10:35:26 +0100

    Moved readFile to own file.

--- cubes/vulkan/include/evulkan.h
+++ cubes/vulkan/include/evulkan.h
@@ -22,24 +22,6 @@
 #include "vertex.h"
 #include "grid.h"
 
-static std::vector<char> readFile(const std::string& filename)
-{
-    std::ifstream file(filename, std::ios::ate | std::ios::binary);
-
-    if (!file.is_open())
-    {
-        throw std::runtime_error("failed to open file.");
-    }
-
-    size_t fileSize = static_cast<size_t>(file.tellg());
-    std::vector<char> buffer(fileSize);
-    file.seekg(0);
-    file.read(buffer.data(), fileSize);
-    file.close();
-    
-    return buffer;
-}
-
 class EVulkan {
 public:
     void run() {
--- cubes/vulkan/src/render.cpp
+++ cubes/vulkan/src/render.cpp
@@ -2,6 +2,7 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#include "util.h"
 
 void EVulkan::createImageViews()
 {
