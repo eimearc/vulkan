b0549caeb8fd1952784b505dec4a9bfe093ad412
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-06-03 14:41:21 +0100

    Removed commented code.

--- include/util.h
+++ include/util.h
@@ -14,24 +14,6 @@ struct UniformBufferObject
     glm::mat4 proj;
 };
 
-// struct QueueFamilyIndices
-// {
-//     std::optional<uint32_t> graphicsFamily;
-//     std::optional<uint32_t> presentFamily;
-
-//     bool isComplete()
-//     {
-//         return graphicsFamily.has_value() && presentFamily.has_value();
-//     }
-// };
-
-// struct SwapChainSupportDetails
-// {
-//     VkSurfaceCapabilitiesKHR capabilities;
-//     std::vector<VkSurfaceFormatKHR> formats;
-//     std::vector<VkPresentModeKHR> presentModes;
-// };
-
 std::vector<char> readFile(const std::string& filename);
 void update(std::vector<Vertex> &vertices, const Grid &grid, size_t startOffset, size_t endOffset);
 UniformBufferObject getUBO(const uint32_t &_width, const uint32_t &_height);
